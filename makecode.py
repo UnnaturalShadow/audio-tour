ec8437f7d73e2ef4d53ae69b048401998f13aacb
UnnaturalShadow <joshuahdecker@gmail.com>
2025-11-18 10:08:39 -0700

    initial commit

---
+++ makecode.py
@@ -0,0 +1,10 @@
+import qrcode
+
+# Data to encode in the QR code
+data = "https://moa.byu.edu"
+
+# Generate the QR code image
+img = qrcode.make(data)
+
+# Save the image to a file
+img.save("my_qrcode.png")
